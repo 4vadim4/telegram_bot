f415eff38bbf20df8ae710f3c5dacaa75f8fc1f6
Vadim Borodich <vborodich@remedypointsolutions.com>
2018-11-30 12:05:09 +0300

    added bot weather

---
+++ telegramweather.py
@@ -0,0 +1,43 @@
+import time
+import telepot
+from telepot.loop import MessageLoop
+import requests
+
+
+TOKEN = '709434769:AAHiglAptFiOEELiwfkbLcF4xWcyvqUUJeo'
+APPID = 'c387e9eac059a7bf80345a729f18ea9c'
+
+bot = telepot.Bot(TOKEN)
+
+
+def get_data(city):
+    request_str = 'http://api.openweathermap.org/data/2.5/weather?q=%s&APPID=c387e9eac059a7bf80345a729f18ea9c' % city
+    response = requests.get(request_str).json()
+    weather_msg = '%s, температура: %s \N{DEGREE SIGN}C, влажность: %d %%, скорость ветра: %d м/с, давление: %.2f мм.рт.ст., %s' % (
+        response['name'], response['main']['temp'] - 273.15, response['main']['humidity'],
+        response['wind']['speed'], response['main']['pressure'] * 0.75, response['weather'][0]['description'])
+
+    return weather_msg
+
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    if content_type == 'text' and msg['text'].lower() == '.погода':
+        weather_msg = get_data('Minsk')
+        bot.sendMessage(chat_id, weather_msg)
+
+    elif content_type == 'text' and msg['text'].lower() == '.погодам':
+        weather_msg = get_data('Moscow')
+        bot.sendMessage(chat_id, weather_msg)
+
+    elif content_type == 'text' and msg['text'].lower() == '.погодал':
+        weather_msg = get_data('Lyubertsy')
+        bot.sendMessage(chat_id, weather_msg)
+
+
+
+MessageLoop(bot, handle).run_as_thread()
+
+# Keep the program running.
+while 1:
+    time.sleep(1)
