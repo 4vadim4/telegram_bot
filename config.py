08a615da1fce23ba625f2540b851d8ed9e6d55cd
Vadim Borodich <vborodich@remedypointsolutions.com>
2018-12-04 12:46:47 +0300

    moved to class

---
+++ config.py
@@ -0,0 +1,15 @@
+token = '709434769:AAHiglAptFiOEELiwfkbLcF4xWcyvqUUJeo'
+
+appid = 'c387e9eac059a7bf80345a729f18ea9c'
+
+chat_id = '-1001138432342'
+
+update_id = 0
+
+weather_json = 'http://api.openweathermap.org/data/2.5/weather?q=%s&APPID=%s'
+
+msg_weather = '%s, температура: %.1f \N{DEGREE SIGN}C, влажность: %d %%, скорость ветра: %d м/с, давление: %.2f мм.рт.ст., %s'
+
+msg_error = 'ВНИМАНИЕ!!!\n Принудительное завершение или сбой на платформе Heroku.'
+
+msg_activity = ' активность в чате за последние 24 часа ( первое: %s ):\n -= %d сообщений =-\n'
--- telegrambot.py
+++ telegrambot.py
@@ -3,94 +3,121 @@ import os
 import ast
 import time
 import telepot
-
+import requests
+from telepot.loop import MessageLoop
 from apscheduler.schedulers.background import BackgroundScheduler
+from config import token, appid, update_id, chat_id, weather_json, msg_weather, msg_error, msg_activity
 
 
-token = '709434769:AAHiglAptFiOEELiwfkbLcF4xWcyvqUUJeo'
-update_id = 0
-TelegramBot = telepot.Bot(token)
+class ActivityInfo(object):
+
+    def __init__(self, token):
+        self.sum_line = 0
+        self.users_dict_activity = dict()
+        self.activity_percent = dict()
+        self.bot = telepot.Bot(token)
+
+    def get_stat_begin_data(self):
+        with open('history.txt', 'r') as tmp_file:
+            first_line = tmp_file.readline()
+
+        record = ast.literal_eval(first_line)
+        begin_data = time.ctime(record['message']['date'])
+        begin_data_time = time.strptime(begin_data)
+        begin_data_str = time.strftime("%d %B %Y - %H:%M:%S", begin_data_time)
+
+        return begin_data_str
+
+    def get_group_history(self, update_id):
+        tmp_history = self.bot.getUpdates(offset=update_id, timeout=60)
+        length_tmp_history = len(tmp_history)
+        next_update_id = tmp_history[-1]['update_id'] + 1 if length_tmp_history != 0 else 0
+
+        with open('history.txt', 'a') as tmp_file:
+            for record in tmp_history:
+                tmp_file.write(str(record) + '\n')
+
+        self.get_group_history(next_update_id) if length_tmp_history == 100 else self.parse_history()
+
+    def parse_history(self):
+        with open('history.txt', 'r') as tmp_file:
+            for line in tmp_file.readlines():
+                self.sum_line += 1
+                record = ast.literal_eval(line)
+
+                if 'edited_message' in record.keys():
+                    record['message'] = record.pop('edited_message')
+                user = record['message']['from']['id']
+
+                if user not in self.users_dict_activity:
+                    self.users_dict_activity[user] = 1
+                else:
+                    self.users_dict_activity[user] += 1
+
+        self.get_activity_persent()
+
+    def get_activity_persent(self):
+        for key, value in self.users_dict_activity.items():
+            percent = value * 100 / self.sum_line
+            self.activity_percent[key] = percent
+
+        self.build_stat_message()
+
+    def build_stat_message(self):
+        user_sort_by_activity = sorted(self.activity_percent.items(), key=lambda kv: kv[1])
+        user_sort_by_activity.reverse()
+        self.msg =  msg_activity % (self.get_stat_begin_data() if self.sum_line != 0 else '- нет активности -', self.sum_line)
+        os.remove('history.txt')
+
+        for user_stat in user_sort_by_activity:
+            user_data = self.bot.getChatMember(chat_id='-1001138432342', user_id=user_stat[0])
+            extra_msg = '- %s %s -> %d сообщений ( %.2f %% )\n' % (user_data['user']['first_name'], user_data['user'].get('last_name', 'Unknown'), self.users_dict_activity.get(user_data['user']['id']), user_stat[1])
+            self.msg += extra_msg
+
+        self.print_msg()
+
+    def print_msg(self):
+        self.bot.sendMessage(chat_id='-1001138432342', text=self.msg)
 
 
-def get_stat_begin_data():
-    with open('history.txt', 'r') as tmp_file:
-        first_line = tmp_file.readline()
+class Weather(object):
 
-    record = ast.literal_eval(first_line)
-    begin_data = time.ctime(record['message']['date'])
-    begin_data_time = time.strptime(begin_data)
-    begin_data_str = time.strftime("%d %B %Y - %H:%M:%S", begin_data_time)
+    def __init__(self, token):
+        self.bot = telepot.Bot(token)
 
-    return begin_data_str
+    def get_data(self, city):
+        response = requests.get(weather_json %(city, appid)).json()
+        return msg_weather % (response['name'], response['main']['temp'] - 273.15, response['main']['humidity'],
+            response['wind']['speed'], response['main']['pressure'] * 0.75, response['weather'][0]['description'])
 
+    def handle(self, msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        if content_type == 'text' and msg['text'].lower() == '.погода':
+            weather_msg = self.get_data('Minsk')
+            self.bot.sendMessage(chat_id, weather_msg)
 
-def get_group_history(update_id):
-    tmp_history = TelegramBot.getUpdates(offset=update_id, timeout=60)
-    length_tmp_history = len(tmp_history)
-    next_update_id = tmp_history[-1]['update_id'] + 1 if length_tmp_history != 0 else 0
+        elif content_type == 'text' and msg['text'].lower() == '.погодам':
+            weather_msg = self.get_data('Moscow')
+            self.bot.sendMessage(chat_id, weather_msg)
 
-    with open('history.txt', 'a') as tmp_file:
-        for record in tmp_history:
-            tmp_file.write(str(record) + '\n')
-
-    get_group_history(next_update_id) if length_tmp_history == 100 else parse_history()
-
-
-def parse_history():
-    sum_line = 0
-    users_dict_activity = dict()
-
-    with open('history.txt', 'r') as tmp_file:
-        for line in tmp_file.readlines():
-            sum_line += 1
-            record = ast.literal_eval(line)
-
-            if 'edited_message' in record.keys():
-                record['message'] = record.pop('edited_message')
-            user = record['message']['from']['id']
-
-            if user not in users_dict_activity:
-                users_dict_activity[user] = 1
-            else:
-                users_dict_activity[user] += 1
-
-    get_activity_persent(users_dict_activity, sum_line)
+        elif content_type == 'text' and msg['text'].lower() == '.погодал':
+            weather_msg = self.get_data('Lyubertsy')
+            self.bot.sendMessage(chat_id, weather_msg)
 
 
-def get_activity_persent(users_dict_activity, sum_line):
-    activity_percent = dict()
-    for key, value in users_dict_activity.items():
-        percent = value * 100 / sum_line
-        activity_percent[key] = percent
-
-    build_stat_message(users_dict_activity, activity_percent, sum_line)
-
-
-def build_stat_message(users_dict_activity, activity_percent, sum_line):
-    user_sort_by_activity = sorted(activity_percent.items(), key=lambda kv: kv[1])
-    user_sort_by_activity.reverse()
-    msg = ' активность в чате за последние 24 часа ( первое: %s ):\n -= %d сообщений =-\n' % (get_stat_begin_data() if sum_line != 0 else '- нет активности -', sum_line)
-    os.remove('history.txt')
-
-    for user_stat in user_sort_by_activity:
-        user_data = TelegramBot.getChatMember(chat_id='-1001138432342', user_id=user_stat[0])
-        extra_msg = '- %s %s -> %d сообщений ( %.2f %% )\n' % (user_data['user']['first_name'], user_data['user'].get('last_name', 'Unknown'), users_dict_activity.get(user_data['user']['id']), user_stat[1])
-        msg += extra_msg
-
-    print_msg(msg)
-
-
-def print_msg(msg):
-    TelegramBot.sendMessage(chat_id='-1001138432342', text=msg)
 
 if __name__ == '__main__':
+    activity = ActivityInfo(token)
+    weather = Weather(token)
     sched = BackgroundScheduler()
 
-    sched.add_job(get_group_history, 'cron', [update_id], hour=2)
+    sched.add_job(activity.get_group_history, 'cron', [update_id], hour=12, minute=50)
     sched.start()
+
+    MessageLoop(weather.bot, weather.handle).run_as_thread()
+
     try:
         while True:
             time.sleep(1)
     except (KeyboardInterrupt, SystemExit):
-        TelegramBot.sendMessage(chat_id='-1001138432342', text='ВНИМАНИЕ!!!\n Принудительное завершение или сбой на платформе Heroku. Сбор статистики остановлен!')
-        # print('qqq')
+        activity.bot.sendMessage(chat_id=chat_id, text=msg_error)
